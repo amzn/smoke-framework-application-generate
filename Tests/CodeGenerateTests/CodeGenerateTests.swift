c287d9ec8495f838ab267ce7be2b4f17f5498df4
Mendez <mndzup@88665a076941.ant.amazon.com>
2021-08-12 16:57:21 -0400

    Edit commands being passed into unit tests

--- Tests/CodeGenerateTests/CodeGenerateTests.swift
+++ Tests/CodeGenerateTests/CodeGenerateTests.swift
@@ -1,59 +1,55 @@
 import XCTest
 @testable import SmokeFrameworkApplicationGenerateCommand
 class CodeGenerateTests: XCTestCase {
-    func testExample() {
-        print("This is an example test")
-    }
 
-    func executionPlanServiceGenerate() throws {
-        let swaggerCommand = ["base-file-path /Users/mndzup/Desktop/Service_Swagger/ExecutionPlanService", "swagger-version 2"]
+    func testExecutionPlanServiceGenerate() {
+        let swaggerCommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_Swagger/ExecutionPlanService", "--swagger-version", "2"]
         SmokeFrameworkApplicationGenerateCommand.main(swaggerCommand)
-        let openAPICommand = ["base-file-path /Users/mndzup/Desktop/Service_OpenAPI/ExecutionPlanService", "swagger-version 3"]
+        let openAPICommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_OpenAPI/ExecutionPlanService", "--swagger-version", "3"]
         SmokeFrameworkApplicationGenerateCommand.main(openAPICommand)
     }
     
-    func contentIngressServiceGenerate() {
-        let swaggerCommand = ["base-file-path /Users/mndzup/Desktop/Service_Swagger/ContentIngressService", "swagger-version 2"]
+    func testContentIngressServiceGenerate() {
+        let swaggerCommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_Swagger/ContentIngressService", "--swagger-version", "2"]
         SmokeFrameworkApplicationGenerateCommand.main(swaggerCommand)
-        let openAPICommand = ["base-file-path /Users/mndzup/Desktop/Service_OpenAPI/ContentIngressService", "swagger-version 3"]
+        let openAPICommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_OpenAPI/ContentIngressService", "--swagger-version", "3"]
         SmokeFrameworkApplicationGenerateCommand.main(openAPICommand)
     }
     
-    func playbackAssetsServiceGenerate() {
-        let swaggerCommand = ["base-file-path /Users/mndzup/Desktop/Service_Swagger/PlaybackAssetsService", "swagger-version 2"]
+    func testPlaybackAssetsServiceGenerate() {
+        let swaggerCommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_Swagger/PlaybackAssetsService", "--swagger-version", "2"]
         SmokeFrameworkApplicationGenerateCommand.main(swaggerCommand)
-        let openAPICommand = ["base-file-path /Users/mndzup/Desktop/Service_OpenAPI/PlaybackAssetsService", "swagger-version 3"]
+        let openAPICommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_OpenAPI/PlaybackAssetsService", "--swagger-version", "3"]
         SmokeFrameworkApplicationGenerateCommand.main(openAPICommand)
     }
     
-    func playbackObjectProcessorServiceGenerate() {
-        let swaggerCommand = ["base-file-path /Users/mndzup/Desktop/Service_Swagger/PlaybackObjectProcessorService", "swagger-version 2"]
+    func testPlaybackObjectProcessorServiceGenerate() {
+        let swaggerCommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_Swagger/PlaybackObjectProcessorService", "--swagger-version", "2"]
         SmokeFrameworkApplicationGenerateCommand.main(swaggerCommand)
-        let openAPICommand = ["base-file-path /Users/mndzup/Desktop/Service_OpenAPI/PlaybackObjectProcessorService", "swagger-version 3"]
+        let openAPICommand = ["--base-file-path", "/Users/mndzup/Desktop/Service_OpenAPI/PlaybackObjectProcessorService", "--swagger-version", "3"]
         SmokeFrameworkApplicationGenerateCommand.main(openAPICommand)
     }
     
-    func playbackObjectServiceGenerate() {
-        let swaggerCommand = ["base-file-path /Users/mndzup/Desktop/Service_Swagger/PlaybackObjectService", "swagger-version 2"]
+    func testPlaybackObjectServiceGenerate() {
+        let swaggerCommand = ["--base-file-path /Users/mndzup/Desktop/Service_Swagger/PlaybackObjectService", "--swagger-version 2"]
         SmokeFrameworkApplicationGenerateCommand.main(swaggerCommand)
-        let openAPICommand = ["base-file-path /Users/mndzup/Desktop/Service_OpenAPI/PlaybackObjectService", "swagger-version 3"]
+        let openAPICommand = ["--base-file-path /Users/mndzup/Desktop/Service_OpenAPI/PlaybackObjectService", "--swagger-version 3"]
         SmokeFrameworkApplicationGenerateCommand.main(openAPICommand)
     }
     
-    func targetPublishingServiceGenerate() {
-        let swaggerCommand = ["base-file-path /Users/mndzup/Desktop/Service_Swagger/TargetPublishingService", "swagger-version 2"]
+    func testTargetPublishingServiceGenerate() {
+        let swaggerCommand = ["--base-file-path /Users/mndzup/Desktop/Service_Swagger/TargetPublishingService", "--swagger-version 2"]
         SmokeFrameworkApplicationGenerateCommand.main(swaggerCommand)
-        let openAPICommand = ["base-file-path /Users/mndzup/Desktop/Service_OpenAPI/TargetPublishingService", "swagger-version 3"]
+        let openAPICommand = ["--base-file-path /Users/mndzup/Desktop/Service_OpenAPI/TargetPublishingService", "--swagger-version 3"]
         SmokeFrameworkApplicationGenerateCommand.main(openAPICommand)
     }
 
     static var allTests = [
-        ("testExample", testExample),
-        ("executionPlanServiceGenerate", executionPlanServiceGenerate),
-        ("contentIngressServiceGenerate", contentIngressServiceGenerate),
-        ("playbackAssetsServiceGenerate", playbackAssetsServiceGenerate),
-        ("playbackObjectProcessorServiceGenerate", playbackObjectProcessorServiceGenerate),
-        ("playbackObjectServiceGenerate", playbackObjectServiceGenerate),
-        ("targetPublishingServiceGenerate", targetPublishingServiceGenerate)
+        ("testExecutionPlanServiceGenerate", testExecutionPlanServiceGenerate),
+        ("testContentIngressServiceGenerate", testContentIngressServiceGenerate),
+        ("testPlaybackAssetsServiceGenerate", testPlaybackAssetsServiceGenerate),
+        ("testPlaybackObjectProcessorServiceGenerate", testPlaybackObjectProcessorServiceGenerate),
+        ("testPlaybackObjectServiceGenerate", testPlaybackObjectServiceGenerate),
+        ("testTargetPublishingServiceGenerate", testTargetPublishingServiceGenerate)
     ]
 }
