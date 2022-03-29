a13d2d72f4e71e7036d2ac4dc56f1ff346d24b0b
Simon Pilkington <simonpi@amazon.com>
2022-03-29 12:37:39 -0700

    Update legacy package manifests.

--- Package@swift-5.4.swift
+++ Package@swift-5.4.swift
@@ -29,22 +29,24 @@ let package = Package(
             targets: ["SmokeFrameworkCodeGeneration"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/amzn/smoke-aws-generate.git", from: "3.0.0-beta.1"),
-        .package(url: "https://github.com/amzn/service-model-swift-code-generate.git", from: "3.0.0-beta.1"),
+        .package(name: "SmokeAWSGenerate",
+                 url: "https://github.com/amzn/smoke-aws-generate.git", from: "3.0.0-beta.1"),
+        .package(name: "ServiceModelSwiftCodeGenerate",
+                 url: "https://github.com/amzn/service-model-swift-code-generate.git", from: "3.0.0-beta.1"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
     ],
     targets: [
         .executableTarget(
             name: "SmokeFrameworkApplicationGenerate", dependencies: [
                 .target(name: "SmokeFrameworkCodeGeneration"),
-                .product(name: "OpenAPIServiceModel", package: "service-model-swift-code-generate"),
+                .product(name: "OpenAPIServiceModel", package: "ServiceModelSwiftCodeGenerate"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ]
         ),
         .target(
             name: "SmokeFrameworkCodeGeneration", dependencies: [
-                .product(name: "ServiceModelGenerate", package: "service-model-swift-code-generate"),
-                .product(name: "SmokeAWSModelGenerate", package: "smoke-aws-generate"),
+                .product(name: "ServiceModelGenerate", package: "ServiceModelSwiftCodeGenerate"),
+                .product(name: "SmokeAWSModelGenerate", package: "SmokeAWSGenerate"),
             ]
         ),
         .testTarget(
